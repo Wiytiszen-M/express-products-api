ede86a88e5c143dd171d4698198d00287788d8f7
MWWtz <martinwiytiszen@gmail.com>
2026-06-07 23:03:31 -0300

    Handle common PostgreSQL errors

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,10 +1,31 @@
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (err, req, res) => {
   console.error(err);
 
   if (err.isOperational) {
     return res.status(err.statusCode).json({
       success: false,
       message: err.message,
+    });
+  }
+
+  if (err.code === "23505") {
+    return res.status(409).json({
+      success: false,
+      message: "Resource already exists",
+    });
+  }
+
+  if (err.code === "23503") {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid related resource",
+    });
+  }
+
+  if (err.code === "22P02") {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid value format",
     });
   }
 
