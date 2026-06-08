07ba1aa849dd05a9e36ff1dda6a9b29e589aa31c
MWWtz <martinwiytiszen@gmail.com>
2026-06-07 23:46:33 -0300

    Hash user passwords with bcrypt

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,8 @@
+const bcrypt = require("bcrypt");
 const userRepository = require("../repositories/user.repository");
 const AppError = require("../errors/AppError");
+
+const SALT_ROUNDS = 10;
 
 const registerUser = async ({ name, email, password }) => {
   const existingUser = await userRepository.findByEmail(email);
@@ -8,10 +11,12 @@ const registerUser = async ({ name, email, password }) => {
     throw new AppError("Email already in use", 409);
   }
 
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
   const newUser = await userRepository.create({
     name,
     email,
-    password,
+    password: hashedPassword,
   });
 
   return newUser;
